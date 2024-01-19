c14c23d23aa2c8a1ce03f23db61a92bbcc6997f7
Anthony <amnaranjos@uce.edu.ec>
2024-01-18 22:19:32 -0500

    Taller 24: Corrección Evaluación Final 1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,122 @@
 <template>
-  <PokemonPages/>
+  <!--PokemonPages/-->
+  <div class="container" v-if="mostrarJuego">
+    <h1>Casino Pokemon</h1>
+    <h2>Puntaje: {{ puntaje }}</h2>
+    <h2>Intentos: {{ intento }}</h2>
+      <Imagen :texto="texto1" :urlImg="url1" />
+      <Imagen :texto="texto2" :urlImg="url2" />
+      <Imagen :texto="texto3" :urlImg="url3" />
+      <button @click="jugar">Jugar</button>
+  </div>
+  <div v-if="mostrarPerdedor">
+    <h1>Has utilizado tu 5 intentos</h1>
+    <h1>El juego ha terminado, intentalo nuevamente</h1>
+    <button @click="reiniciar">Nuevo juego</button>
+  </div>
+  <div v-if="mostrarGanador">
+    <h1>Puntaje: {{ puntaje }}</h1>
+    <h1>Felicitaciones has ganado un premio de $10.000,00</h1>
+    <button @click="reiniciar">Nuevo juego</button>
+  </div>
 </template>
 
 <script>
 import PokemonPages from "./pages/PokemonPage.vue";
+import Imagen from "./components/Imagen.vue";
 
 export default {
   name: "App",
   components: {
-    PokemonPages,
+    /*PokemonPages,*/
+    Imagen,
+  },
+  data() {
+    return {
+      puntaje: 0,
+      intento: 0,
+      url1: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+      url2: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+      url3: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+      texto1: "xxxxxxxxx",
+      texto2: "xxxxxxxxx",
+      texto3: "xxxxxxxxx",
+      mostrarGanador: false,
+      mostrarPerdedor: false,
+      mostrarJuego: true,
+    };
+  },
+  methods: {
+    async jugar() {
+      this.texto1 = "charmander";
+      //instancia 1
+      const data1 = await this.consumirAPI();
+      this.texto1 = data1.answer;
+      this.url1 = data1.image;
+      const data2 = await this.consumirAPI();
+      this.texto2 = data2.answer;
+      this.url2 = data2.image;
+      const data3 = await this.consumirAPI();
+      this.texto3 = data3.answer;
+      this.url3 = data3.image;
+
+      this.evaluarResultado();
+    },
+    async consumirAPI() {
+      return await fetch("https://yesno.wtf/api").then((r) => r.json());
+    },
+    evaluarResultado() {
+      this.intento++;
+      if (
+        this.texto1 === "yes" &&
+        this.texto2 === "yes" &&
+        this.texto3 === "yes"
+      ) {
+        this.puntaje += 5;
+      } else if (
+        (this.texto1 === "yes" && this.texto2 === "yes") ||
+        (this.texto1 === "yes" && this.texto3 === "yes") ||
+        (this.texto3 === "yes" && this.texto2 === "yes")
+      ) {
+        this.puntaje += 2;
+      } else if (
+        this.texto1 === "yes" ||
+        this.texto2 === "yes" ||
+        this.texto3 === "yes"
+      ) {
+        this.puntaje += 1;
+      } else {
+        this.puntaje += 0;
+      }
+
+      if (this.puntaje >= 10) {
+        this.mostrarGanador = true;
+        this.mostrarJuego = false;
+        this.mostrarPerdedor = false;
+      }
+
+      if (this.intento === 5) {
+        this.mostrarPerdedor = true;
+        this.mostrarJuego = false;
+        this.mostrarGanador = false;
+      }
+    },
+    reiniciar() {
+      this.puntaje = 0;
+      this.intento = 0;
+      this.url1 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg";
+      this.url2 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg";
+      this.url3 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg";
+      this.texto1 = "xxxxxxxxx";
+      this.texto2 = "xxxxxxxxx";
+      this.texto3 = "xxxxxxxxx";
+      this.mostrarGanador = false;
+      this.mostrarPerdedor = false;
+      this.mostrarJuego = true;
+    },
   },
 };
 </script>
@@ -22,4 +130,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.container {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  grid-template-columns: repeat(4, 200px);
+}
+h1 {
+  grid-column: span 2;
+}
+
 </style>
